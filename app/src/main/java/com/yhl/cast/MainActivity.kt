7921ee2f2d9456eaf106e786de3187171ebe3a12
honglieyuan <honglieyuan@163.com>
2020-11-20 22:51:06 +0800

    1.Android 9.0 请求运行时存储权限 2.Android 9.0 明文传输限制 3.Glide 加载图片 4.添加VideoView 5.调整布局

--- app/src/main/java/com/yhl/cast/MainActivity.kt
+++ app/src/main/java/com/yhl/cast/MainActivity.kt
@@ -3,13 +3,15 @@ package com.yhl.cast
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
+import com.yanzhenjie.permission.AndPermission
+import com.yanzhenjie.permission.runtime.Permission
 import com.yhl.http.HttpClient
 import com.yhl.server.FILE_SERVER_PORT
 import com.yhl.server.HttpService
 import com.yhl.server.MESSAGE_SERVER_PORT
-import com.yhl.util.getIPv4Address
 import io.reactivex.Observable
 import io.reactivex.ObservableEmitter
 import io.reactivex.android.schedulers.AndroidSchedulers
@@ -18,10 +20,10 @@ import kotlinx.android.synthetic.main.activity_main.*
 import okhttp3.Request
 
 
-
 class MainActivity : AppCompatActivity() {
     private val TAG = MainActivity::class.simpleName
-    val host: String = "localhost"
+
+    val host: String = "127.0.0.1"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -37,9 +39,25 @@ class MainActivity : AppCompatActivity() {
             requestMessageServer()
         }
 
-        btnRequestFile.setOnClickListener {
+        btnRequestImage.setOnClickListener {
+            ivImage.visibility = View.VISIBLE
+            vvVideo.visibility = View.GONE
             Glide.with(this).load(getImageUrl()).into(ivImage)
         }
+
+        btnRequestVideo.setOnClickListener{
+            vvVideo.visibility = View.VISIBLE
+            ivImage.visibility = View.GONE
+            vvVideo.setVideoPath(getVideoUrl())
+            vvVideo.start()
+        }
+
+        AndPermission.with(this)
+                .runtime()
+                .permission(Permission.Group.STORAGE)
+                .onGranted { Log.i(TAG, "RequestStorage: onGranted") }
+                .onDenied{ Log.i(TAG, "RequestStorage: onDenied")}
+                .start()
     }
 
     private fun requestMessageServer() {
@@ -64,5 +82,9 @@ class MainActivity : AppCompatActivity() {
 
     private fun getFileServerUrl() = "http://$host:${FILE_SERVER_PORT}"
 
-    private fun getImageUrl() = "${getFileServerUrl()}/sdcard/galaxy.jpg"
+    private fun getImageUrl() = "${getFileServerUrl()}/sdcard/download/sky.jpg"
+    private fun getVideoUrl() = "${getFileServerUrl()}/sdcard/download/sky.jpg"
+
+    private fun getRemoteImageUrl() = "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1605889829717&di=60255c3716346be17816b81ae1c64a02&imgtype=0&src=http%3A%2F%2Fimg1.gtimg.com%2Fhn%2Fpics%2Fhv1%2F51%2F217%2F2144%2F139468986.jpg"
+    private fun getRemoteVideoUrl() = "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1605889829717&di=60255c3716346be17816b81ae1c64a02&imgtype=0&src=http%3A%2F%2Fimg1.gtimg.com%2Fhn%2Fpics%2Fhv1%2F51%2F217%2F2144%2F139468986.jpg"
 }
